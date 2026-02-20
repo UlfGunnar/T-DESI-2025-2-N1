d8b537cc6114f46dcc44727ccf4b4a6d04ed3c17
UlfGunnar <ulfgunnar6@gmail.com>
2026-02-19 21:14:06 -0300

    Modelagem da Classe

---
+++ Modelagem_classe.py
@@ -0,0 +1,17 @@
+'''
+Planejamento de como a classe será feita na SA
+
+Hospital -> Paciente 
+    CPF - Identificador único
+    Nome_Completo - Identificação secundaria
+    Cor_pulseira - Para saber a velocidade do atendimento
+    Tem_Alergia - Para selecionamento de remédios
+    Tem_Convênio - Para verificar se há desconto 
+
+    Fazer_Cadastro
+
+    Fazer_Triagem()
+
+    Receitar_Remedios()
+
+'''
