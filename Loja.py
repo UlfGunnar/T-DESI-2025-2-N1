f633c1b5cb050328a1f174dd6277d02f962bce73
UlfGunnar <ulfgunnar6@gmail.com>
2026-03-03 20:16:42 -0300

    Finalizado sistema de loja do CS

--- Loja.py
+++ Loja.py
@@ -1,13 +1,21 @@
 # Programa que simula uma loja onde você pode adicionar um item nela
 
 import os
+import time
 
-def Cadastrar():
+def Cadastrar_Produto():
+    print('='*30)
     nome = str(input("Qual arma você quer adicionar?: "))
     quant = int(input("Quantos teremos no estoque?: ")) 
     valor = int(input("E quanto custará?: R$"))
 
     Estoque.setdefault(f"{nome}", [quant, valor, nome])
+    Carrinho.setdefault(f"{nome}", [0])
+
+    os.system('cls')
+    print('Produto adicionado!')
+    time.sleep(1)
+    os.system('cls')
 
 Carrinho = {
     "AK-47": 0,
@@ -17,69 +25,91 @@ Carrinho = {
 }
 
 Estoque = {
-    "AK-47": [21,2700,"AK47"],
+    "AK-47": [21,2700,"AK-47"],
     "M4A1": [9, 2900,"M4A1"],
     "AWP": [26, 4750,"AWP"]
 }
 
 while True:
-    #os.system('cls')
+    os.system('cls')
     print('[1] - Mostrar produtos')
     print('[2] - Cadastrar Produtos')
     print('[3] - Pagar')
-    print('SAIR para desligar o app')
-    
-    escolha_menu = input("Opção: ")
+
+    escolha_menu = int(input("Opção: "))
     
     #os.system('cls')
     match escolha_menu:
         
-        case "1":
-                for c, produtos in enumerate(Estoque.values()):
+        #========================#
+        #====MOSTRAR PRODUTOS====#
+        #========================#
+
+        case 1: 
+                print("="*30)
+                print("O que você quer comprar?")
+                for c, produtos in enumerate(Estoque.values()): # Mostra os produtos
                     print(f"[{c+1}] - {produtos[2]}")
 
-                print('VOLTAR para ir ao menu')
-                escolha_produto = input('Opção: ')
-
+                print('[0] - Voltar')
+                escolha_produto = int(input('Opção: '))
                 
-                if type(escolha_produto) == str:
-                    print('thau')
-                    continue    
+                if escolha_produto == 0: 
+                    continue
                 
-                else:
-                    print('oieeeeeeee')
-                    for c, produtos in enumerate(Estoque.values()):
-                        if (c + 1) == escolha_produto :
-                            print('oi')
-                            input()
-
-                            """
-                            if produtos[1] > 0:
-                                print(f'Temos {produtos[2]} Ak-47 cada uma por R${produtos[1]}')
+                elif escolha_produto > 0:
+                    for c, produtos in enumerate(Estoque.values()): # Checa qual produto o usuário escolheu
+                        if (c + 1) == escolha_produto:
+                            if produtos[1] > 0: # Verifica se tem item no inventário 
+                                print("="*30)
+                                print(f'Temos {produtos[2]} cada uma por R${produtos[1]}')
                                 Confirmacao = str(input("Você tem certeza? [sim/nao]: ")).lower()
 
-                            if Confirmacao == "sim":
-                                produtos[0] -= 1
-                                Carrinho[f"{produtos[2]}"] += 1
-                                Carrinho["Total_Pagar"] += produtos[1]
+                                if Confirmacao == "sim":
+                                    produtos[0] -= 1                        # Diminui a quantidade no estoque
+                                    Carrinho[f"{produtos[2]}"] += 1         # Adiciona no historico o item comprado
+                                    Carrinho["Total_Pagar"] += produtos[1]  # Adiciona o valor do item no total
 
-                                #os.system('cls')
-                                print(f"Aqui já embrulhamos sua {produtos[2]}!")
-                                continue
+                                    os.system('cls')
+                                    print(f"Compra confirmada!")
+                                    time.sleep(1)
+                                    os.system('cls')
+                                    continue
 
-                            elif Confirmacao == "nao":
-                                #os.system('cls')
-                                continue
+                                else:
+                                    print("ERROR VALOR INVÁLIDO! Encerrando app >:(")
+                                    break
                 
-                            else:
-                                print("ERROR VALOR INVÁLIDO! >:(")
-                                break
-                            """
-"""
+                else: 
+                    print("ERROR VALOR INVÁLIDO! Encerrando app >:(")
+                    break
 
-print('Você comprou: \n' \
-     f'{Carrinho["AK-47"]} - AK47\n'
-     f'{Carrinho["M4A1"]} - M4A1\n'
-     f'{Carrinho["AWP"]} - AWP\n'
-     f'Total a Pagar: R${Carrinho["Total_Pagar"]}')
-"""
+        #==========================#
+        #====CADASTRAR PRODUTOS====#
+        #==========================#
+
+        case 2:
+            Cadastrar_Produto()
+            continue
+
+        #=================#
+        #====PAGAMENTO====#
+        #=================#
+
+        case 3:
+            print('='*30)
+            print("Você comprou: ")
+
+            for produtos in Estoque.values():
+                if Carrinho[produtos[2]] > 0: 
+                    print(f"[{produtos[2]}] {Carrinho[produtos[2]]} vezes")
+
+            print(f"Total a pagar: R${Carrinho['Total_Pagar']}")
+            print('='*30)
+            escolha_pagamento = str(input('Pagar? [sim/nao]: ')).lower()
+            
+            if escolha_pagamento == "sim":
+                os.system('cls')
+                print("Pagamento confirmado! Encerrando o app...")
+                time.sleep(1)
+                break
